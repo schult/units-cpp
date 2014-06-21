c4655b667bf94cabc2520e07b6500127b594efbc
Steven Schulteis <schult@gmail.com>
2014-06-20 22:57:56 -0500

    Whitespace.

--- test.cpp
+++ test.cpp
@@ -21,6 +21,7 @@ class Dimension
 public:
     Dimension() = delete;
     constexpr Dimension( const Dimension& ) = default;
+
     constexpr Dimension( const FundamentalDimension& dim ) :
         m_numer_length( dim == FundamentalDimension::LENGTH ? 1 : 0 ),
         m_numer_mass( dim == FundamentalDimension::MASS ? 1 : 0 ),
@@ -36,11 +37,13 @@ public:
     }
 
     Dimension& operator=( const Dimension& ) = default;
+
     Dimension& operator*=( const Dimension& rhs )
     {
         *this = *this * rhs;
         return *this;
     }
+
     Dimension& operator/=( const Dimension& rhs )
     {
         *this = *this / rhs;
@@ -60,6 +63,7 @@ public:
             lhs.m_denom_current == rhs.m_denom_current &&
             lhs.m_denom_temperature == rhs.m_denom_temperature;
     }
+
     friend bool operator!=( const Dimension& lhs, const Dimension& rhs )
     {
         return !(lhs == rhs);
@@ -80,6 +84,7 @@ public:
             lhs.m_denom_current + rhs.m_denom_current,
             lhs.m_denom_temperature + rhs.m_denom_temperature );
     }
+
     friend constexpr Dimension operator/(
         const Dimension& lhs, const Dimension& rhs )
     {
@@ -119,6 +124,7 @@ private:
         m_numer_time( reduced( n_time, d_time ) ),
         m_numer_current( reduced( n_current, d_current ) ),
         m_numer_temperature( reduced( n_temperature, d_temperature ) ),
+
         m_denom_length( reduced( d_length, n_length ) ),
         m_denom_mass( reduced( d_mass, n_mass ) ),
         m_denom_time( reduced( d_time, n_time ) ),
@@ -144,6 +150,7 @@ constexpr Dimension operator*( const Dimension& lhs, const FundamentalDimension&
 {
     return lhs * Dimension( rhs );
 }
+
 constexpr Dimension operator/( const Dimension& lhs, const FundamentalDimension& rhs )
 {
     return rhs / Dimension( rhs );
