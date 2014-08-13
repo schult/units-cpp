bac5d8c30f8ca729c21a14f058ff49e382378024
Steven Schulteis <schult@gmail.com>
2014-08-12 22:06:56 -0500

    Make Dimension comparison operators constexpr.

--- src/Dimension.cpp
+++ src/Dimension.cpp
@@ -12,25 +12,6 @@ Dimension& Dimension::operator/=( const Dimension& rhs )
     return *this;
 }
 
-bool operator==( const Dimension& lhs, const Dimension& rhs )
-{
-    return lhs.m_numer_length == rhs.m_numer_length &&
-        lhs.m_numer_mass == rhs.m_numer_mass &&
-        lhs.m_numer_time == rhs.m_numer_time &&
-        lhs.m_numer_current == rhs.m_numer_current &&
-        lhs.m_numer_temperature == rhs.m_numer_temperature &&
-        lhs.m_denom_length == rhs.m_denom_length &&
-        lhs.m_denom_mass == rhs.m_denom_mass &&
-        lhs.m_denom_time == rhs.m_denom_time &&
-        lhs.m_denom_current == rhs.m_denom_current &&
-        lhs.m_denom_temperature == rhs.m_denom_temperature;
-}
-
-bool operator!=( const Dimension& lhs, const Dimension& rhs )
-{
-    return !(lhs == rhs);
-}
-
 std::ostream& operator<<( std::ostream& stream, const Dimension& dim )
 {
     auto repeat = [](std::ostream& s, const char *str, int n) {
--- src/Dimension.h
+++ src/Dimension.h
@@ -49,8 +49,10 @@ public:
     Dimension& operator*=( const Dimension& rhs );
     Dimension& operator/=( const Dimension& rhs );
 
-    friend bool operator==( const Dimension& lhs, const Dimension& rhs );
-    friend bool operator!=( const Dimension& lhs, const Dimension& rhs );
+    friend constexpr bool operator==(
+        const Dimension& lhs, const Dimension& rhs );
+    friend constexpr bool operator!=(
+        const Dimension& lhs, const Dimension& rhs );
 
     friend constexpr Dimension operator*(
         const Dimension& lhs, const Dimension& rhs );
@@ -104,6 +106,25 @@ private:
     unsigned int m_denom_current;
     unsigned int m_denom_temperature;
 };
+
+constexpr bool operator==( const Dimension& lhs, const Dimension& rhs )
+{
+    return lhs.m_numer_length == rhs.m_numer_length &&
+        lhs.m_numer_mass == rhs.m_numer_mass &&
+        lhs.m_numer_time == rhs.m_numer_time &&
+        lhs.m_numer_current == rhs.m_numer_current &&
+        lhs.m_numer_temperature == rhs.m_numer_temperature &&
+        lhs.m_denom_length == rhs.m_denom_length &&
+        lhs.m_denom_mass == rhs.m_denom_mass &&
+        lhs.m_denom_time == rhs.m_denom_time &&
+        lhs.m_denom_current == rhs.m_denom_current &&
+        lhs.m_denom_temperature == rhs.m_denom_temperature;
+}
+
+constexpr bool operator!=( const Dimension& lhs, const Dimension& rhs )
+{
+    return !(lhs == rhs);
+}
 
 constexpr Dimension operator*( const Dimension& lhs, const Dimension& rhs )
 {
